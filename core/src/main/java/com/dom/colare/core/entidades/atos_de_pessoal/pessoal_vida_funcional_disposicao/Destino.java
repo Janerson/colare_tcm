578d7b6a545a3a520260d57d096075765cf98002
Janerson Douglas <douglas.janerson@gmail.com>
2020-01-10 23:54:59 -0300

    Mapeamento PessoalVidaFuncionalDisposicao

--- core/src/main/java/com/dom/colare/core/entidades/atos_de_pessoal/pessoal_vida_funcional_disposicao/Destino.java
+++ core/src/main/java/com/dom/colare/core/entidades/atos_de_pessoal/pessoal_vida_funcional_disposicao/Destino.java
@@ -1,9 +1,11 @@
 
 package com.dom.colare.core.entidades.atos_de_pessoal.pessoal_vida_funcional_disposicao;
 
+import com.dom.colare.core.entidades.shared.BaseEntityID;
+import lombok.Data;
+
+import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
@@ -12,7 +14,9 @@ import java.util.Map;
  * Informar o destino da disposição
  * 
  */
-public class Destino {
+@Entity
+@Data
+public class Destino extends BaseEntityID {
 
     /**
      * Informar a esfera de disposição do servidor, conforme tabela.
@@ -20,106 +24,39 @@ public class Destino {
      * 
      */
     @NotNull
-    public Destino.CodEsferaDisposicao codEsferaDisposicao;
+    private Integer codEsferaDisposicao;
     /**
      * Codificação IBGE do Ente Federado
      * (Required)
      * 
      */
     @NotNull
-    public String codIBGE;
+    private String codIBGE;
     /**
      * Informa o tipo de entidade que recepcionara o servidor.
      * (Required)
      * 
      */
     @NotNull
-    public Destino.TipoEntidade tipoEntidade;
+    private Integer tipoEntidade;
     /**
      * Informar o nome do Órgão Ou Entidade que recepcionara o servidor a disposição.
      * (Required)
      * 
      */
     @NotNull
-    public String nomeOrgaoOuEntidade;
+    private String nomeOrgaoOuEntidade;
     /**
      * Informa o numero do CNPJ do Órgão ou entidade que recepcionara o servidor a disposição.
      * (Required)
      * 
      */
     @NotNull
-    public String numeroCNPJ;
+    private String numeroCNPJ;
     /**
      * ID da Unidade Gestora conforme cadastro no sistema Passaporte
      * 
      */
-    public Integer idUnidadeGestora;
-
-    public enum CodEsferaDisposicao {
-
-        _1(1),
-        _2(2),
-        _3(3);
-        private final Integer value;
-        private final static Map<Integer, CodEsferaDisposicao> CONSTANTS = new HashMap<Integer, CodEsferaDisposicao>();
-
-        static {
-            for (CodEsferaDisposicao c: values()) {
-                CONSTANTS.put(c.value, c);
-            }
-        }
-
-        private CodEsferaDisposicao(Integer value) {
-            this.value = value;
-        }
-
-        public Integer value() {
-            return this.value;
-        }
-
-        public static CodEsferaDisposicao fromValue(Integer value) {
-            CodEsferaDisposicao constant = CONSTANTS.get(value);
-            if (constant == null) {
-                throw new IllegalArgumentException((value +""));
-            } else {
-                return constant;
-            }
-        }
-
-    }
-
-    public enum TipoEntidade {
-
-        _1(1),
-        _2(2),
-        _3(3),
-        _4(4);
-        private final Integer value;
-        private final static Map<Integer, TipoEntidade> CONSTANTS = new HashMap<Integer, TipoEntidade>();
-
-        static {
-            for (TipoEntidade c: values()) {
-                CONSTANTS.put(c.value, c);
-            }
-        }
-
-        private TipoEntidade(Integer value) {
-            this.value = value;
-        }
-
-        public Integer value() {
-            return this.value;
-        }
-
-        public static TipoEntidade fromValue(Integer value) {
-            TipoEntidade constant = CONSTANTS.get(value);
-            if (constant == null) {
-                throw new IllegalArgumentException((value +""));
-            } else {
-                return constant;
-            }
-        }
-
-    }
+    private Integer idUnidadeGestora;
 
 }
--- core/src/main/java/com/dom/colare/core/entidades/atos_de_pessoal/pessoal_vida_funcional_disposicao/PessoalVidaFuncionalDisposicao.java
+++ core/src/main/java/com/dom/colare/core/entidades/atos_de_pessoal/pessoal_vida_funcional_disposicao/PessoalVidaFuncionalDisposicao.java
@@ -1,8 +1,10 @@
 
 package com.dom.colare.core.entidades.atos_de_pessoal.pessoal_vida_funcional_disposicao;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.dom.colare.core.entidades.shared.BaseEntityID;
+import lombok.Data;
+
+import javax.persistence.Entity;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -13,7 +15,9 @@ import javax.validation.constraints.NotNull;
  * Dados de Disposição (Movimentação Externa)
  * 
  */
-public class PessoalVidaFuncionalDisposicao {
+@Entity
+@Data
+public class PessoalVidaFuncionalDisposicao extends BaseEntityID {
 
     /**
      * Código do Tipo de Envio
@@ -21,47 +25,47 @@ public class PessoalVidaFuncionalDisposicao {
      * 
      */
     @NotNull
-    public PessoalVidaFuncionalDisposicao.CodTipoEnvio codTipoEnvio;
+    private Integer codTipoEnvio;
     /**
      * Descreve o motivo da Atualização ou Correção
      * 
      */
-    public String motivoAtualizacaoCorrecao;
+    private String motivoAtualizacaoCorrecao;
     /**
      * ID da Admissão Previamente Informada
      * (Required)
      * 
      */
     @NotNull
-    public Integer idPessoalAdmissao;
+    private Integer idPessoalAdmissao;
     /**
      * Data de Inicio
      * (Required)
      * 
      */
     @NotNull
-    public String dataInicio;
+    private String dataInicio;
     /**
      * Data fim
      * (Required)
      * 
      */
     @NotNull
-    public String dataFinal;
+    private String dataFinal;
     /**
      * Informa o tipo de disposição
      * (Required)
      * 
      */
     @NotNull
-    public PessoalVidaFuncionalDisposicao.CodTipoDisposicao codTipoDisposicao;
+    private Integer codTipoDisposicao;
     /**
      * ID do arquivo enviado contendo o documento digitalizado
      * (Required)
      * 
      */
     @NotNull
-    public String idDocumentoPDF;
+    private String idDocumentoPDF;
     /**
      * destino
      * <p>
@@ -71,78 +75,7 @@ public class PessoalVidaFuncionalDisposicao {
      */
     @Valid
     @NotNull
-    public Destino destino;
+    private Destino destino;
 
-    public enum CodTipoDisposicao {
-
-        _1(1),
-        _2(2),
-        _3(3),
-        _4(4),
-        _5(5),
-        _6(6),
-        _7(7),
-        _8(8),
-        _9(9);
-        private final Integer value;
-        private final static Map<Integer, CodTipoDisposicao> CONSTANTS = new HashMap<Integer, CodTipoDisposicao>();
-
-        static {
-            for (CodTipoDisposicao c: values()) {
-                CONSTANTS.put(c.value, c);
-            }
-        }
-
-        private CodTipoDisposicao(Integer value) {
-            this.value = value;
-        }
-
-        public Integer value() {
-            return this.value;
-        }
-
-        public static CodTipoDisposicao fromValue(Integer value) {
-            CodTipoDisposicao constant = CONSTANTS.get(value);
-            if (constant == null) {
-                throw new IllegalArgumentException((value +""));
-            } else {
-                return constant;
-            }
-        }
-
-    }
-
-    public enum CodTipoEnvio {
-
-        _1(1),
-        _2(2),
-        _3(3);
-        private final Integer value;
-        private final static Map<Integer, CodTipoEnvio> CONSTANTS = new HashMap<Integer, CodTipoEnvio>();
-
-        static {
-            for (CodTipoEnvio c: values()) {
-                CONSTANTS.put(c.value, c);
-            }
-        }
-
-        private CodTipoEnvio(Integer value) {
-            this.value = value;
-        }
-
-        public Integer value() {
-            return this.value;
-        }
-
-        public static CodTipoEnvio fromValue(Integer value) {
-            CodTipoEnvio constant = CONSTANTS.get(value);
-            if (constant == null) {
-                throw new IllegalArgumentException((value +""));
-            } else {
-                return constant;
-            }
-        }
-
-    }
-
+  
 }
