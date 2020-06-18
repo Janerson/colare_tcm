728d4ca888cb71ad8120019d713c49d6f03ef83d
Janerson <douglas.janerson@gmail.com>
2020-06-18 10:48:47 -0300

    add prod properties

--- api/src/main/java/com/dom/colare/api/exception/ApiError.java
+++ api/src/main/java/com/dom/colare/api/exception/ApiError.java
@@ -3,7 +3,7 @@ package com.dom.colare.api.exception;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -23,6 +23,6 @@ public class ApiError {
         super();
         this.status = status;
         this.message = message;
-        errors = Arrays.asList(error);
+        errors = Collections.singletonList(error);
     }
 }
