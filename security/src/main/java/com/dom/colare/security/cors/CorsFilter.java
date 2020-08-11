a58ae172f98aa05a30320e1515a2cbc3796d5aff
Janerson <douglas.janerson@gmail.com>
2020-08-11 18:36:47 -0300

    application-dev.properties

--- security/src/main/java/com/dom/colare/security/cors/CorsFilter.java
+++ security/src/main/java/com/dom/colare/security/cors/CorsFilter.java
@@ -24,7 +24,6 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) resp;
         response.setHeader("Access-Control-Allow-Origin", origin);
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Allow-Origin-With-Credentials", "true");
         if("OPTIONS".equals(request.getMethod())){
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
